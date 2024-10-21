073845b3817eb0103033f16190a7c3c8d26558bb
ChaithraS1719 <chaithra68338@gmail.com>
2024-10-21 11:37:17 +0530

    add 21-10-2024

---
+++ ArtificialPlants.java
@@ -0,0 +1,13 @@
+class ArtificialPlants 
+     {
+	     public static void main(String plants[])
+		    {
+			  System.out.println("About item");
+			  System.out.println("Perfect Decoration: These artificial plants come in different styles, each designed to blend seamlessly into any decor, adding a cozy and natural touch to your home or office");
+			  System.out.println("High-Quality Material: Made from durable plastic, these faux plants are realistic, lush, and vibrant. The pots are covered with a layer of green grass, enhancing their natural look");
+			  System.out.println("Maintenance-Free: No upkeep or attention is needed for these plastic plants; they'll stay vibrant and fresh without fading. Just occasionally wipe them down with a damp cloth");
+			  System.out.println("Versatile Use: Suitable for indoor and outdoor decoration, these potted plants can be placed on windowsills, desks, bookshelves, bathrooms, kitchens, and more, creating a bright and clean style");
+			  System.out.println("Warm Notice: These plants may have a slight odour; it's recommended to leave them in a ventilated area before use. Due to packaging, the leaves might be slightly compressed—simply adjust them for the best display");
+			  System.out.println("Product Dimensions : 5 cms X 13 cms");
+			}
+	 }
